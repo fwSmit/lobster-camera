45a3ea4d97af56e3eddf68dee035619bedd0b7d1
fwsmit <fw.smit01@gmail.com>
2020-07-22 21:30:15 +0200

    removed old code

--- start_recording.py
+++ start_recording.py
@@ -6,20 +6,7 @@ from Camera import Camera, Resolutions
 # print openCV version
 print("Using openCV version ", cv2.__version__)
 
-#  frame_width = 640
-#  frame_height = 480
-#  framerate = 30
-#  frame_width = 1920
-#  frame_height = 1080
-#  framerate = 60.0
-frame_width = 1280
-frame_height = 720
-framerate = 60.0
-
-#  camera = Camera(frame_width, frame_height, framerate, VideoFormats.mjpeg)
-
 camera = Camera.fromEnum(Resolutions.RAW_480_60)
-
 
 while(True):
     ret = camera.captureFrame()
